c20716513033ff30d807137cbf76dbc32d22523e
leonaugustine <laugustine1@gmail.com>
2016-05-15 03:32:39 +1000

    SQL Modification
    allows multiple room in booking
    MUST CHANGE DERBY IMP IO
    remove field in booking dto
    add field to room dto ( booking id which it refers too, can be null if
    not booked)
    
    Then in booking dto add arraylist of rooms in the booking, this is
    obtained by cycling thru all rooms and getting current booking id in
    room id

--- Assign2/sql/query.sql
+++ Assign2/sql/query.sql
@@ -1,3 +1,4 @@
 SELECT * FROM CUSTOMER;
 SELECT r.id, r.hotel_fk, rt.price, rt.bedType, rt.numBeds , rt.name FROM room r, room_type rt, hotel h
 	WHERE r.room_type_fk = rt.id AND r.hotel_fk=h.id;
+	
