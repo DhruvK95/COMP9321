DhruvK95/COMP9321

commit f85e17e34b5c71ef5118d6ba43624a297f6f0990

Allows for discounts to be implemented
includes sample insert on single room for 30 percent in hotel 1

Assign2/sql/schema.sql
@@ -104,9 +104,9 @@ CREATE TABLE booking_on_rooms
 
 
 CREATE TABLE discount (
-  id              INT NOT NULL,
+  id              INT NOT NULL GENERATED ALWAYS AS IDENTITY,
   room_type_fk    INTEGER NOT NULL,
-  discount_price  INTEGER NOT NULL,
+  discount_price  FLOAT NOT NULL,
   hotel_fk        INTEGER NOT NULL,
   start_date      DATE NOT NULL,
   end_date        DATE NOT NULL,
@@ -115,6 +115,9 @@ CREATE TABLE discount (
   CONSTRAINT discount_HOTEL_ID_FK FOREIGN KEY (hotel_fk) REFERENCES hotel(id),
   PRIMARY KEY (id)
 );
+
+INSERT INTO discount VALUES (DEFAULT, 1, 30, 1, '2016-01-01', '2016-01-02');
+
 
 --two hotels in each city, suburb included in name
 INSERT INTO hotel VALUES (DEFAULT,'Parramatta Star ','Sydney');

Assign2/sql/query.sql
@@ -1,5 +1,5 @@
-SELECT * FROM ROOM;
+SELECT * FROM discount;
 SELECT r.id, r.hotel_fk, rt.price, rt.bedType, rt.numBeds , rt.name FROM room r, room_type rt, hotel h
 	WHERE r.room_type_fk = rt.id AND r.hotel_fk=h.id;
-	
+SELECT d.id, rt.name, d.discount_price, d.hotel_fk, d.start_date, d.end_date FROM discount d ,room_type rt WHERE d.room_type_fk=rt.id;
 SELECT r.id FROM BOOKING_ON_ROOMS bor, BOOKING b, ROOM r WHERE bor.booking_fk=b.id AND r.id = bor.room_fk AND bor.booking_fk=..;

Assign2/src/mvcController/HotelController.java
@@ -19,6 +19,7 @@ import mvcModel.BookingDTO;
 import mvcModel.CustomerDTO;
 import mvcModel.DBStorageDTO;
 import mvcModel.DerbyDAOImpl;
+import mvcModel.DiscountDTO;
 import mvcModel.HotelDTO;
 import mvcModel.RoomDTO;
 
@@ -53,6 +54,8 @@ public class HotelController extends HttpServlet {
 			for(BookingDTO b : database.getAllBookings()){
 				database.addRoomsToBooking(cast.getRoomAssociationsID(b.getId()), b.getId() );
 			}
+			database.addAllDiscounts(cast.initDiscounts());
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -177,7 +180,7 @@ public class HotelController extends HttpServlet {
 			 			ArrayList<HotelDTO> tempSave = database.getAllHotels();
 			 			for ( HotelDTO h : tempSave ){
 			 				System.out.println( h.getId() + " " + h.getHotelName() + " " + h.getLocation());
-			 				System.out.println("----------------------------------------------------------------------");
+			 				System.out.println("--------------------------------h--------------------------------------");
 			 				for( RoomDTO r : h.getRooms()){
 			 					System.out.println( "      " + r.getName() + " " + r.getId()+ " " +r.getNumBeds()+ " " +r.getParentHotelID()+ " " +r.getPrice());
 			 				}
@@ -186,16 +189,20 @@ public class HotelController extends HttpServlet {
 			 			ArrayList<CustomerDTO> tempSave2 = database.getAllCustomers();
 			 			for ( CustomerDTO c : tempSave2 ){
 			 				System.out.println( c.getId() + " " + c.getUser_name()+ " " + c.getPassword()+ " " + c.getFirst_name()+ " " + c.last_name+ " " + c.getEmail()+ " " + c.getAddress()+ " " + c.getCc_number()+ " " + c.getCc_name()+ " " + c.getCc_expiry());
-			 				System.out.println("----------------------------------------------------------------------");
+			 				System.out.println("--------------------------------c--------------------------------------");
 			 			}
 			 			ArrayList<BookingDTO> tempSave3 = database.getAllBookings();
 			 			for ( BookingDTO b : tempSave3 ){
 			 				System.out.println( b.getId() + " " + b.getStartDate() + " " + b.getEndDate() + " " + b.getCustomerID());
-			 				System.out.println("-------------s--------------------------------------------------------");
+			 				System.out.println("---------------------------------b-------------------------------------");
 			 				for(RoomDTO r : b.getAllRooms()){
 			 					System.out.println( "      " + r.getName() + " " + r.getId()+ " " +r.getNumBeds()+ " " +r.getParentHotelID()+ " " +r.getPrice());
 			 				}
-
+			 			}
+			 			ArrayList<DiscountDTO> tempSave4 = database.getAllDiscounts();
+			 			for ( DiscountDTO d : tempSave4 ){
+			 				System.out.println("----------------------------------d------------------------------------");			 				
+			 				System.out.println(d.getId() + " "+ d.getTypeOfRoom() + " " + d.getParentHotelID() + " " + d.getStartDate() + " " +d.getEndDate());
 			 			}
 			 			//TESTING HOTELS WORKS
 			System.out.print("ERERERERERERERERERERERERERERERER");

Assign2/src/mvcModel/DBStorageDTO.java
@@ -6,7 +6,8 @@ public class DBStorageDTO {
 	public ArrayList<HotelDTO> hotels;
 	public ArrayList<CustomerDTO> customers;
 	public ArrayList<BookingDTO> bookings;
-	
+	public ArrayList<DiscountDTO> discounts;
+
 	public DBStorageDTO() {
 		super();
 		this.hotels = new ArrayList<HotelDTO>();
@@ -88,5 +89,12 @@ public class DBStorageDTO {
 		return null;
 	}
 	
+	public void addAllDiscounts(ArrayList<DiscountDTO> aD){
+		this.discounts = aD;
+	}
+	
+	public ArrayList<DiscountDTO> getAllDiscounts(){
+		return this.discounts;
+	}
 
 }

Assign2/src/mvcModel/DerbyDAOImpl.java
@@ -192,6 +192,38 @@ public class DerbyDAOImpl  {
 		}
 		return hotelsMod;
 	}
+	
+	public ArrayList<DiscountDTO> initDiscounts(){
+		ArrayList<DiscountDTO> discounts = new ArrayList<DiscountDTO>();
+		try{
+			Statement stmnt = connection.createStatement();
+			String query_cast = "SELECT d.id, rt.name, d.discount_price, d.hotel_fk, d.start_date, d.end_date FROM discount d ,room_type rt WHERE d.room_type_fk=rt.id";
+			ResultSet res = stmnt.executeQuery(query_cast);
+			logger.info("The result set size is "+res.getFetchSize());
+			while(res.next()){
+				DiscountDTO currDiscount = new DiscountDTO();
+				currDiscount.setId(res.getInt("id"));
+				currDiscount.setTypeOfRoom(res.getString("name"));
+				currDiscount.setDiscountPercent(res.getFloat("discount_price"));
+				currDiscount.setParentHotelID(res.getInt("hotel_fk"));
+				currDiscount.setStartDate(res.getDate("start_date"));
+				currDiscount.setEndDate(res.getDate("end_date"));
+				
+				discounts.add(currDiscount);
+			}
+			res.close();
+			stmnt.close();
+			
+		}catch(Exception e){
+			System.out.println("Caught Exception");
+			e.printStackTrace();
+		}
+		return discounts;
+	}
+
+	
+	
+	
 	//Check if valid uName and pass (Customers)
 	public boolean login(String user, String pass){
 		Statement stmnt;

Assign2/src/mvcModel/DiscountDTO.java
@@ -1,0 +1,50 @@
+package mvcModel;
+
+import java.sql.Date;
+
+
+public class DiscountDTO {
+	public int id;
+	public String typeOfRoom;
+	public float discountPercent;
+	public int parentHotelID;
+	public Date startDate;
+	public Date endDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTypeOfRoom() {
+		return typeOfRoom;
+	}
+	public void setTypeOfRoom(String typeOfRoom) {
+		this.typeOfRoom = typeOfRoom;
+	}
+	public int getParentHotelID() {
+		return parentHotelID;
+	}
+	public void setParentHotelID(int parentHotelID) {
+		this.parentHotelID = parentHotelID;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date date) {
+		this.startDate = date;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public float getDiscountPercent() {
+		return discountPercent;
+	}
+	public void setDiscountPercent(float discountPercent) {
+		this.discountPercent = discountPercent;
+	}
+}

commit 1d50c6d05b68bf730b5748216b2665f2879857a5

Clean up staff schema and create some sample staff acounts. Fix error caused in f85e17e

Assign2/sql/schema.sql
@@ -2,7 +2,6 @@
 -- Control Shift R to run in IntelliJ
 ALTER TABLE ROOM DROP CONSTRAINT ROOM_HOTEL_ID_FK;
 ALTER TABLE ROOM DROP CONSTRAINT ROOM_ROOM_TYPE_ID_FK;
-
 ALTER TABLE DISCOUNT DROP CONSTRAINT DISCOUNT_HOTEL_ID_FK;
 ALTER TABLE DISCOUNT DROP CONSTRAINT DISCOUNT_ROOM_TYPE_FK;
 ALTER TABLE BOOKING DROP CONSTRAINT BOOKING_CUSTOMER_ID_FK;
@@ -11,14 +10,14 @@ ALTER TABLE BOOKING_ON_ROOMS DROP CONSTRAINT ROOM_BOOKING_ID_FK;
 
 -- Since the booking relation changed, when you next regenerate the database you will need to drop the old constraint and 
 -- add the new one. In other words, before you run the script next, change ROOM to HOTEL on the next line, and change it back after.
-DROP TABLE BOOKING_ON_ROOMS;
-DROP TABLE DISCOUNT;
-DROP TABLE BOOKING;
-DROP TABLE ROOM;
-DROP TABLE ROOM_TYPE;
 DROP TABLE HOTEL;
 DROP TABLE CUSTOMER;
 DROP TABLE STAFF;
+DROP TABLE ROOM_TYPE;
+DROP TABLE BOOKING;
+DROP TABLE ROOM;
+DROP TABLE BOOKING_ON_ROOMS;
+DROP TABLE DISCOUNT;
 
 
 CREATE TABLE hotel (
@@ -27,7 +26,6 @@ CREATE TABLE hotel (
   location			VARCHAR(40) NOT NULL,
   PRIMARY KEY (id)
 );
-
 
 
 CREATE TABLE customer (
@@ -45,14 +43,15 @@ CREATE TABLE customer (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE staff
-(
-  id              INTEGER PRIMARY KEY NOT NULL,
+
+CREATE TABLE staff (
+  id              INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
   first_name      VARCHAR(20),
   last_name       VARCHAR(20),
   username        VARCHAR(50) NOT NULL UNIQUE,
-  password        VARCHAR(100) NOT NULL,
-  staff_class     VARCHAR(20) NOT NULL
+  password        VARCHAR(40) NOT NULL,
+  is_owner        boolean,
+  PRIMARY KEY (id)
 );
 
 
@@ -67,16 +66,15 @@ CREATE TABLE room_type (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE booking
-(
+
+CREATE TABLE booking (
   id              INTEGER PRIMARY KEY NOT NULL NOT NULL GENERATED ALWAYS AS IDENTITY,
   start_date      DATE NOT NULL,
   end_date        DATE NOT NULL,
   customer_fk     INTEGER,
   CONSTRAINT valid_booking_dates CHECK (start_date <= end_date),
   CONSTRAINT booking_CUSTOMER_ID_fk FOREIGN KEY (customer_fk) REFERENCES CUSTOMER (ID)
-
-  );
+);
   
 
 CREATE TABLE room (
@@ -89,6 +87,7 @@ CREATE TABLE room (
   CONSTRAINT room_HOTEL_ID_FK FOREIGN KEY (hotel_fk) REFERENCES hotel (id)
 );
 
+
 CREATE TABLE booking_on_rooms
 (
  id					INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
@@ -97,10 +96,7 @@ CREATE TABLE booking_on_rooms
 
  CONSTRAINT room_ID_FK FOREIGN KEY (room_fk) REFERENCES room (id),
  CONSTRAINT room_BOOKING_ID_FK FOREIGN KEY (booking_fk) REFERENCES booking (id)
-
 );
-
-
 
 
 CREATE TABLE discount (
@@ -115,8 +111,6 @@ CREATE TABLE discount (
   CONSTRAINT discount_HOTEL_ID_FK FOREIGN KEY (hotel_fk) REFERENCES hotel(id),
   PRIMARY KEY (id)
 );
-
-INSERT INTO discount VALUES (DEFAULT, 1, 30, 1, '2016-01-01', '2016-01-02');
 
 
 --two hotels in each city, suburb included in name
@@ -138,19 +132,18 @@ INSERT INTO hotel VALUES (DEFAULT,'Leabrook Amazement','Adelaide');
 INSERT INTO hotel VALUES (DEFAULT,'BridgeWater Horror','Hobart');
 INSERT INTO hotel VALUES (DEFAULT,'Howden Horror','Hobart');
 
-
 INSERT INTO customer VALUES (DEFAULT,'LeonisCool','123', 'Leon','Augustine', 'laugustine1@gmail.com', 'pert', 123456, 'Leon', '4/4/2015', true );
 INSERT INTO customer VALUES (DEFAULT,'EdlanisShit','123', 'Edlan', 'Policarpio','eldanpolicarpioschool@gmail.com', 'hobar', 123456, 'Edlan', '4/4/2015', true);
 
+INSERT INTO staff VALUES(DEFAULT, 'Mister', 'Manager', 'manager1', 'password', false);
+INSERT INTO staff VALUES(DEFAULT, 'Madame', 'Owner', 'owner1', 'password', true);
 
 --Make Room Types
-
 INSERT INTO room_type VALUES (DEFAULT,  80, FALSE, 1, 'Single');
 INSERT INTO room_type VALUES (DEFAULT, 130, FALSE, 2, 'Twin');
 INSERT INTO room_type VALUES (DEFAULT, 150, TRUE, 1, 'Queen');
 INSERT INTO room_type VALUES (DEFAULT, 200, TRUE, 1, 'Executive');
 INSERT INTO room_type VALUES (DEFAULT, 320, TRUE, 2, 'Suite');
-
 
 --Single Rooms (Hotels 1 - 8, odd rooms have 2)
 INSERT INTO room VALUES (DEFAULT, 1, 1);
@@ -167,8 +160,6 @@ INSERT INTO room VALUES (DEFAULT, 1, 7);
 INSERT INTO room VALUES (DEFAULT, 1, 8);
 INSERT INTO room VALUES (DEFAULT, 1, 9);
 INSERT INTO room VALUES (DEFAULT, 1, 9);
-
-
 
 --Double Rooms (Even num Hotels)
 INSERT INTO room VALUES (DEFAULT, 2, 2);
@@ -214,3 +205,5 @@ INSERT INTO booking_on_rooms VALUES (DEFAULT, 20, 2);
 INSERT INTO booking_on_rooms VALUES (DEFAULT, 28, 2);
 INSERT INTO booking_on_rooms VALUES (DEFAULT, 11, 2);
 INSERT INTO booking_on_rooms VALUES (DEFAULT, 9, 2);
+
+INSERT INTO discount VALUES (DEFAULT, 1, 30, 1, '2016-01-01', '2016-01-02');