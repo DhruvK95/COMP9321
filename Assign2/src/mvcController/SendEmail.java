63a48232abf0c50a6d93a4741e5338fe425e5228
EdlanPolicarpio <EdlanPolicarpio@users.noreply.github.com>
2016-05-18 02:40:05 +1000

    BookingEmail

--- Assign2/src/mvcController/SendEmail.java
+++ Assign2/src/mvcController/SendEmail.java
@@ -11,46 +11,85 @@ import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 
 public class SendEmail {
+	private String username;
+	private String password;
+	private Properties props;
+	
+	public SendEmail() {
 
-	public SendEmail(String user, String recipientMail, HttpServletRequest request) {
+		username = "makehotelsgreatagain@gmail.com";
+		password = "m4ke_h0tels_gr8";
 
-		final String username = "makehotelsgreatagain@gmail.com";
-		final String password = "m4ke_h0tels_gr8";
-
-		Properties props = new Properties();
+		props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
 
+		
+	}
+	
+	public void verificationMail(String user, String recipientMail, HttpServletRequest request){
 		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
-			}
-		  });
+				  new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+				  });
 
-		try {
+				try {
 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("makehotelsgreatagain@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse(recipientMail));
-			message.setSubject("Testing Subject");
-			String serverName = request.getServerName();
-			int portNumber = request.getServerPort();
-			String contextPath = request.getContextPath();
-			System.out.println(serverName + ":" +portNumber + contextPath );
-			String path = serverName.concat(":").concat(Integer.toString(portNumber)).concat(contextPath);
-			message.setText("Please verify your account given the following link "+
-					"http://" + path + "?verify="+user+"&action=login");
+					Message message = new MimeMessage(session);
+					message.setFrom(new InternetAddress("makehotelsgreatagain@gmail.com"));
+					message.setRecipients(Message.RecipientType.TO,
+						InternetAddress.parse(recipientMail));
+					message.setSubject("Verification");
+					String serverName = request.getServerName();
+					int portNumber = request.getServerPort();
+					String contextPath = request.getContextPath();
+					System.out.println(serverName + ":" +portNumber + contextPath );
+					String path = serverName.concat(":").concat(Integer.toString(portNumber)).concat(contextPath);
+					message.setText("Please verify your account given the following link "+
+							"http://" + path + "?verify="+user+"&action=login");
 
-			Transport.send(message);
+					Transport.send(message);
 
-			System.out.println("Done");
+					System.out.println("Done");
 
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
+				} catch (MessagingException e) {
+					throw new RuntimeException(e);
+				}
+	}
+	
+	public void bookingMail(int bID, String recipientMail, HttpServletRequest request){
+		Session session = Session.getInstance(props,
+				  new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+				  });
+
+				try {
+
+					Message message = new MimeMessage(session);
+					message.setFrom(new InternetAddress("makehotelsgreatagain@gmail.com"));
+					message.setRecipients(Message.RecipientType.TO,
+						InternetAddress.parse(recipientMail));
+					message.setSubject("Verification");
+					String serverName = request.getServerName();
+					int portNumber = request.getServerPort();
+					String contextPath = request.getContextPath();
+					System.out.println(serverName + ":" +portNumber + contextPath );
+					String path = serverName.concat(":").concat(Integer.toString(portNumber)).concat(contextPath);
+					message.setText("Check you booking through the following link"+
+							"http://" + path + "?booking="+bID+"&action=checkBooking");
+
+					Transport.send(message);
+
+					System.out.println("Done");
+
+				} catch (MessagingException e) {
+					throw new RuntimeException(e);
+				}
 	}
 }
